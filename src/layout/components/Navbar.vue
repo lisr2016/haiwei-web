4c61036b88d319507d728b64c1e2e4b5882c1edd
lishaorong <mbigbug@163.com>
2020-09-21 22:40:17 +0800

    update

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80" class="user-avatar" alt="">
+          <img src="../../2061600684590_.pic_hd.jpg" class="user-avatar" alt="">
           <span class="name">{{ name }}</span>
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
